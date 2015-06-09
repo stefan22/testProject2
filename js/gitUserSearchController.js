1d70dd1c307337b6961715a57ee276743e6ac6f3
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-09 15:24:19 +0100

    httpBackend and flush()

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,26 +1,13 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', [ '$resource',function($resource) {
+
+  var searchResource = $resource('https://api.github.com/search/users');
+
   var self = this;
 
-  self.doSearch = function() {
-    console.log(self.searchTerm);
-
-      self.searchResult = {
-        "items": [
-          {
-            "login": "tansaku",
-            "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-            "html_url": "https://github.com/tansaku"
-          },
-          {
-            "login": "stephenlloyd",
-            "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-            "html_url": "https://github.com/stephenlloyd"
-          }
-        ]
-      };
+  self.doSearch = function (){
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm }
+    );
   };
-
-
-
 
 }]); //GitUserSearchController
